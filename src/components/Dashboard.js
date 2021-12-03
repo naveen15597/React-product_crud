e22642282b64ac00ae27a9d17c0f9f98a3c3172c
naveen15597 <63700657+naveen15597@users.noreply.github.com>
2021-12-04 00:38:56 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,7 +13,7 @@ function Dashboard(props) {
 
                                     {/* <!-- Page Heading --> */}
                                     <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                                        <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                                        <h1 className="h3 mb-0 text-gray-800" style={{color:'white'}}>Dashboard</h1>
                                         <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                                 className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                                     </div>
